70dd72d8f58b3ce52d796ab5ade5d446ff400da4
mykolasbauza <mykolas.bauza@gmail.com>
2019-03-04 11:48:41 +0000

    initial commit

---
+++ footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+?>
+ 
+        </div><!-- #content -->
+ 
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="wrap">
+                <?php
+                get_template_part( 'template-parts/footer/footer', 'widgets' );
+ 
+                if ( has_nav_menu( 'social' ) ) : ?>
+                    <nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+                        <?php
+                            wp_nav_menu( array(
+                                'theme_location' => 'social',
+                                'menu_class'     => 'social-links-menu',
+                                'depth'          => 1,
+                                'link_before'    => '<span class="screen-reader-text">',
+                                'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+                            ) );
+                        ?>
+                    </nav><!-- .social-navigation -->
+                <?php endif;
+ 
+                get_template_part( 'template-parts/footer/site', 'info' );
+ 
+                wp_nav_menu( array( 'theme_location' => 'footer-menu' ) );
+                ?>
+            </div><!-- .wrap -->
+        </footer><!-- #colophon -->
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+    </div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+ 
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+
+			<header id="masthead" class="site-header" role="banner">
+				<div class="site-branding">
+				
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<div class="logo">
+                    <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <a href="http://localhost:3000/quotes-on-dev/"> 
+                    <img  src="<?php echo get_template_directory_uri(); ?>/images/qod-logo.svg" class="main-logo" alt="logo">
+                    </a>
+                    </div>
+				</div><!-- .site-branding -->
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
+
+			<div id="content" class="site-content">
